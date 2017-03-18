0f8304b71cd94cde7fea80ada35fb8013fe452e5
Christopher Ryan <ckr5@njit.edu>
2017-03-18 16:33:26 -0400

    added queries for second reuirement for midterm

--- Status.php
+++ Status.php
@@ -2,12 +2,14 @@
 
 require('DatabaseLogin.php');
 
-$sql ="Select DATE_ARRIVED, IN_STOCK, DEAD, SOLD From Plants Where '$_Post[Plantid]' = ID";
+
+$sql ="Select Plant_Name, DATE_ARRIVED, IN_STOCK, DEAD, SOLD From Plants Where ID='{$_POST[Plantid]}'";
 
 if ($conn->query($sql) == TRUE) {
 	$result = $conn->query($sql);
 	if ($result->num_rows > 0){
 	while($row = $result->fetch_assoc()) {
+	echo 'Name: ' .$row["Plant_Name"]."<br>";
 	echo 'Date_Arrived: ' .$row["DATE_ARRIVED"]."<br>";
 	echo 'Number of Plants in Stock: ' .$row["IN_STOCK"]."<br>";
 	echo 'Number of Plants Dead: ' .$row["DEAD"]."<br>";
--- StockOfPlants.php
+++ StockOfPlants.php
@@ -5,14 +5,16 @@
 
 require('DatabaseLogin.php');
 
-$sql ="Select Plant_Name, IN_STOCK FROM Plants Where IN_Stock > 0";
+$sql ="Select Plant_Name, IN_STOCK, ID FROM Plants Where IN_Stock > 0";
 
 if ($conn->query($sql) == TRUE) {
 	$result = $conn->query($sql);
 	if ($result->num_rows > 0){
 	while($row = $result->fetch_assoc()) {
+	echo 'ID: ' .$row["ID"]."<br>";
 	echo 'Plant: ' .$row["Plant_Name"]."<br>";
-	echo 'Stock Count: ' .$row["IN_STOCK"]."<br>"; 
+	echo 'Stock Count: ' .$row["IN_STOCK"]."<br>";
+	echo "<br>"; 
 	
 }
 }
