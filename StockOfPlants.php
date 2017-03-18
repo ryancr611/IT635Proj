cd111f796a3726481f6d37bef7265cdd730503ac
Christopher Ryan <ckr5@njit.edu>
2017-03-18 14:23:05 -0400

    changed sql to include how many of specified plant is in stock

--- StockOfPlants.php
+++ StockOfPlants.php
@@ -5,13 +5,14 @@
 
 require('DatabaseLogin.php');
 
-$sql ="Select Plant_Name FROM Plants Where IN_Stock > 0";
+$sql ="Select Plant_Name, IN_STOCK FROM Plants Where IN_Stock > 0";
 
 if ($conn->query($sql) == TRUE) {
 	$result = $conn->query($sql);
 	if ($result->num_rows > 0){
 	while($row = $result->fetch_assoc()) {
-	echo 'Plant: ' .$row["Plant_Name"]."<br>"; 
+	echo 'Plant: ' .$row["Plant_Name"]."<br>";
+	echo 'Stock Count: ' .$row["IN_STOCK"]."<br>"; 
 	
 }
 }
