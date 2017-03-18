d15c3988f4ee8e1a3b291a5cd8b720b951c2fd2e
Christopher Ryan <ckr5@njit.edu>
2017-03-18 15:22:20 -0400

    Basic Login Authentication

---
+++ login.php
@@ -0,0 +1,31 @@
+<?php
+require('DatabaseLogin.php');
+$table_user = "";
+$table_pass = "";
+$username = ($_POST['username']);
+$password = ($_POST['password']);
+
+
+$sql = "Select Username, Password From Admin";
+
+if ($conn->query($sql) == TRUE) {
+	$result = $conn->query($sql);
+	if ($result->num_rows > 0){
+	while($row = $result->fetch_assoc()) 
+	{
+		$table_user = $row['Username'];
+
+		$table_pass = $row['Password'];
+	}
+	
+	if(($username == $table_user) && ($password == $table_pass))
+	{
+	header("location: home.html");
+	}
+	else
+	{
+	echo "Invalid Login Try Again";
+	}
+}
+}
+?>
